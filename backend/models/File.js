860342579da7697fa56893bab0304640a273e99d
PrathamPShetty <prathampshetty99sai@gmail.com>
2025-05-04 21:33:43 +0530

    deleted

--- backend/models/File.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const fileSchema = new mongoose.Schema({
-    filename: { type: String, required: true },
-    ipfsHash: { type: String, required: true },
-    uploadedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("File", fileSchema);
